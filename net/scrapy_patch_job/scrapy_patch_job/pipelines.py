32f6a709e99833c1567dc3623d63ef2c622b60c3
BobMu <1224357941@qq.com>
2017-11-23 13:02:04 +0800

    工作11.21

--- net/scrapy_patch_job/scrapy_patch_job/pipelines.py
+++ net/scrapy_patch_job/scrapy_patch_job/pipelines.py
@@ -21,7 +21,7 @@ class ScrapyPatchJobPipeline(object):
         time_format = time.strftime('%Y_%m_%d', time.localtime(time.time()))
         absPath = os.path.abspath('..')
         # self.file_name=absPath+'/scrapy_patch_job/zhaopin.xls'
-        self.file_name =absPath + '/scrapy_patch_job/招聘(知春路地铁站五公里内)_' + time_format + '.xls'
+        self.file_name =absPath + '/scrapy_patch_job/招聘_' + time_format + '.xls'
         self.excel_row=0
         self.excel_column=0
         self.excel_work = xlwt.Workbook(encoding='utf-8')
@@ -46,7 +46,7 @@ class ScrapyPatchJobPipeline(object):
 
 
     def process_item(self, item, spider):
-        is_in_range = self.gaode.isInRange(self.my_position, self.my_range, self.gaode.getPosition(item['location']))
+        # is_in_range = self.gaode.isInRange(self.my_position, self.my_range, self.gaode.getPosition(item['location']))
         # is_title = self.filter.filterTitle(item['title'])
         is_title = self.filter.filterTitleOut(item['title'])
         is_location = self.filter.filterLocation(item['location'])
@@ -54,8 +54,8 @@ class ScrapyPatchJobPipeline(object):
         is_treatment = self.filter.filterTreatment(item['treatment'])
         is_description = self.filter.filterDescription(item['description'])
 
-        if is_in_range&is_title&is_location&is_money&is_treatment&is_description:
-        # if is_title&is_location&is_money&is_treatment&is_description:
+        # if is_in_range&is_title&is_location&is_money&is_treatment&is_description:
+        if is_title&is_location&is_money&is_treatment&is_description:
             self.excel_sheet.write(self.excel_row, 0, item['title'])
             self.excel_sheet.write(self.excel_row, 1, item['company'])
             self.excel_sheet.write(self.excel_row, 2, item['city'])
